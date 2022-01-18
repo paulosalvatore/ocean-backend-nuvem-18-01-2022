b634bf73c0a734b69534032b2c6ca71f2cbb53ad
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-18 13:56:57 -0300

    `processs` renomeado para `process`

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function main() {
         res.send("Item removido com sucesso.");
     });
 
-    app.listen(processs.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
 }
 
 main();
