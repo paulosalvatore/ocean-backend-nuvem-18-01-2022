c92f0a879f25e250c6b80ff354fefa00283a7603
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2022-01-18 14:51:17 -0300

    reativando BD e adicionando url do MongoDB

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-// const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+/*const url = "mongodb://localhost:27017";*/
+const url = "mongodb+srv://leliasalles:@Lel788%@cluster0.1rfif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-/*
+
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-*/
-    const collection = undefined;
-    // Aplicação em Express
+
+       // Aplicação em Express
 
     const app = express();
 
