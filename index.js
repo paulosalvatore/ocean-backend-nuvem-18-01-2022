4b93cfa1eaeb885fcad20dd3e776e50250fe7587
Bruno Rezende <97972966+brezende1@users.noreply.github.com>
2022-01-18 15:00:42 -0300

    Alterando senha user admin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb+srv://admin:admin@ocean-backend-18-01-202.g0olq.mongodb.net/";
+const url = "mongodb+srv://admin:ATDeLcHZ0xHW7RmU@ocean-backend-18-01-202.g0olq.mongodb.net/";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
