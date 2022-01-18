ce71dfdc5d2834aafda907a8c9b3cb15591ed043
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-18 15:03:12 -0300

    Parênteses removido

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function main() {
         res.send("Item removido com sucesso.");
     });
 
-    app.listen(process.env.PORT || 3000);)
+    app.listen(process.env.PORT || 3000);
 }
 
 main();
