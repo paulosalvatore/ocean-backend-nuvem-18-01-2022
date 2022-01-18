670c63e69e91c5f35dc3c5556e54665204746935
Paulo Deyvid <88635207+PDEYVID@users.noreply.github.com>
2022-01-18 13:47:01 -0300

    Banco com banco desativa e porta adicionada

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-
+/*
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-
+*/
+    const collection = undefined;
     // Aplicação em Express
 
     const app = express();
@@ -86,7 +87,7 @@ async function main() {
         res.send("Item removido com sucesso.");
     });
 
-    app.listen(3000);
+    app.listem(processs.env.PORT || 3000);
 }
 
 main();
