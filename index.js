0ddc83d473f1c8baa37b6d090d4de6d9071e172b
Erik Amorim <97449583+AmorimErik@users.noreply.github.com>
2022-01-18 14:56:40 -0300

    Correcao da url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb+srv://admin:2cvM5Bn7MQP6wVm@cluster0.4jpf4.mongodb.net/";
+const url = "mongodb+srv://admin:pr1Tv0Eb374526@cluster0.4jpf4.mongodb.net/";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
